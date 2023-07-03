c63f85746a2e088cab1b94f23a6917de864228c7
Aman Yadav <66021025+aman10yadav@users.noreply.github.com>
2023-07-03 11:21:37 +0530

    Adding Prime number program

---
+++ PrimeNumberCheck.py
@@ -0,0 +1,14 @@
+# A number is prime if it is only divisible by itself and 1
+numberInput = int(input("Enter a number to check whether it is prime or not : "))
+
+startingNumber = 2
+
+# start checking from number 2 to the input number
+while (startingNumber * startingNumber) <= numberInput:
+    if numberInput % startingNumber == 0:
+        print(f"The number '{numberInput}' is not a prime number !!")
+        break
+    steps += 1
+    startingNumber += 1
+else :
+    print(f"The number '{numberInput}' is a prime number !!")
